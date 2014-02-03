eb92dde30a4e4f526e77de1efcf3d32c063f9943
EdwinOtten <edwin@salescare.nl>
2014-02-03 11:05:59 +0100

    Created London and Amsterdam decorators

--- SimpleDigitalWatchDecorator/src/simpledigitalwatchdecorator/ClockView.java
+++ SimpleDigitalWatchDecorator/src/simpledigitalwatchdecorator/ClockView.java
@@ -272,34 +272,34 @@ public class ClockView extends javax.swing.JFrame {
 
     private void jCheckBox2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jCheckBox2ActionPerformed
         if (jCheckBox2.isSelected() == true) {
-            clock2 = new LeetDecorator(clock2);
+            clock2 = new LeetDecorator(new SimpleClock());
         } else {
-            clock2 = new WhiteDecorator(clock2);
+            clock2 = new WhiteDecorator(new SimpleClock());
         }
     }//GEN-LAST:event_jCheckBox2ActionPerformed
 
     private void jCheckBox1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jCheckBox1ActionPerformed
         if (jCheckBox1.isSelected() == true) {
-            clock1 = new LeetDecorator(clock1);
+            clock1 = new LeetDecorator(new SimpleClock());
         } else {
-            clock1 = new WhiteDecorator(clock1);
+            clock1 = new WhiteDecorator(new SimpleClock());
         }
     }//GEN-LAST:event_jCheckBox1ActionPerformed
 
     private void jCheckBox3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jCheckBox3ActionPerformed
         if (jCheckBox3.isSelected() == true) {
-            clock3 = new LeetDecorator(clock3);
+            clock3 = new LeetDecorator(new SimpleClock());
         } else {
-            clock3 = new WhiteDecorator(clock3);
+            clock3 = new WhiteDecorator(new SimpleClock());
         }
     }//GEN-LAST:event_jCheckBox3ActionPerformed
 
     private void jRadioButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jRadioButton1ActionPerformed
-        clock1 = new TimezoneDecorator(clock1);
+        clock1 = new LondonDecorator(new SimpleClock());
     }//GEN-LAST:event_jRadioButton1ActionPerformed
-
+//
     private void jRadioButton2ActionPerformed(java.awt.event.ActionEvent evt) {                                              
-        clock1 = new TimezoneDecorator(clock1);
+        clock1 = new AmsterdamDecorator(new SimpleClock());
     } 
     
     // Variables declaration - do not modify//GEN-BEGIN:variables
--- SimpleDigitalWatchDecorator/src/simpledigitalwatchdecorator/LondonDecorator.java
+++ SimpleDigitalWatchDecorator/src/simpledigitalwatchdecorator/LondonDecorator.java
@@ -2,7 +2,6 @@ package simpledigitalwatchdecorator;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.TimeZone;
 import javax.swing.JTextPane;
@@ -11,20 +10,19 @@ import javax.swing.JTextPane;
  *
  * @author EdwinOtten
  */
-public class TimezoneDecorator extends ClockDecorator {
+public class LondonDecorator extends ClockDecorator {
 
-    public TimezoneDecorator(Clock specialClock) {
+    public LondonDecorator(Clock specialClock) {
         super(specialClock);
     }
 
     @Override
     public void showClock(JTextPane jTextPane) {
         DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
-        dateFormat.setTimeZone(TimeZone.getTimeZone("Asia/Calcutta"));
+        dateFormat.setTimeZone(TimeZone.getTimeZone("Europe/London"));
         String time = dateFormat.format(new Date());
         
         jTextPane.setText(time);
-        System.out.println("set by TimezoneDecorator: "+time);
         
         specialClock.showClock(jTextPane);
     }
